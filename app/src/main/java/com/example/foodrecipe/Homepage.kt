05f83bab503de0e28dfe346fe3a3d2750ac16928
Vishal <vishalgoswami165342@gmail.com>
2024-01-18 15:21:24 +0530

    1. splash screen addded with the help of handler() 2. welcome page added 3. Homepage added containing some views in it 4. Work with roomDatabase to show item in recycler view

--- app/src/main/java/com/example/foodrecipe/Homepage.kt
+++ app/src/main/java/com/example/foodrecipe/Homepage.kt
@@ -2,10 +2,45 @@ package com.example.foodrecipe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.foodrecipe.databinding.ActivityHomepageBinding
 
 class Homepage : AppCompatActivity() {
+    lateinit var binding: ActivityHomepageBinding
+    lateinit var rvAdapter: popularAdapter
+    lateinit var dataList: ArrayList<recipe>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_homepage)
+        binding = ActivityHomepageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpRecyclerView()
+
+    }
+
+    private fun setUpRecyclerView() {
+        dataList = ArrayList()
+
+        binding.rvPopular.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+        var db = Room.databaseBuilder(this,AppDatabase::class.java,"db_name")
+            .allowMainThreadQueries()
+            .fallbackToDestructiveMigration()
+            .createFromAsset("recipe.db")
+            .build()
+
+       var daoObject=db.getDao()
+        var recipes=daoObject.getAll()
+
+        for (i in recipes!!.indices){
+            if(recipes[i]!!.category.contains("Popular")){
+                dataList.add(recipes[i]!!)
+            }
+            rvAdapter = popularAdapter(dataList,this)
+            binding.rvPopular.adapter = rvAdapter
+        }
+
     }
 }
